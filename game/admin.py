3904e0a34c217aa9fee1929d56669e53abcfdbf2
Łukasz Leszko <leszko.lucas@gmail.com>
2022-09-06 21:31:17 +0200

    Add admin panel

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import WaitingListModel, GameSessionModel 
+
+admin.site.register(WaitingListModel)
+admin.site.register(GameSessionModel)
