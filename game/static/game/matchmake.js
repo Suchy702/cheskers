f48501a8956aa700f88f79f16ba35ffbe9ab76d1
Łukasz Leszko <leszko.lucas@gmail.com>
2022-09-06 18:25:44 +0200

    Add matchmaking

--- game/static/game/matchmake.js
+++ game/static/game/matchmake.js
@@ -1,5 +1,42 @@
-let button = document.getElementById('matchmake')
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
-button.addEventListener('click', async () => {
-    console.log("co tam")
+let button = document.getElementById("matchmake")
+
+button.addEventListener("click", async () => {
+    const csrftoken = getCookie("csrftoken");
+    let request = new Request(
+        "/game/add_to_waiting_list/",
+        {headers: {'X-CSRFToken': csrftoken}}
+    );
+    let data = await fetch(request, {
+        method: "POST",
+        credentials: "include"
+    });
+    console.log(data);
+    request = new Request(
+        "/game/try_to_pair/",
+        {headers: {'X-CSRFToken': csrftoken}}
+    );
+    data = await fetch(request, {
+        method: "POST",
+        credentials: "include",
+    });
+
+    if (data.redirected)
+        window.location.href = data.url;
+    console.log(data)
 });
--- game/views.py
+++ game/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+import random
 
+from django.db import transaction
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic.base import View, TemplateView
+from django.core.exceptions import ObjectDoesNotExist
+from django.urls import reverse
+
+from .models import WaitingListModel, GameSessionModel
 
 class MatchmakeView(TemplateView):
     template_name = "game/matchmake.html"
+
+class WaitingList(View):
+    def post(self, request):
+        request.session['id'] = random.randint(0, 1000000000)
+        record = WaitingListModel(client_id=request.session['id'])
+        record.save()
+        return HttpResponse(status=200)
+
+class PairMaker(View):
+    @transaction.atomic
+    def post(self, request):
+        current_client = WaitingListModel.objects.get(client_id=request.session['id'])
+        current_client.delete()
+        #WaitingListModel.objects.all().delete()
+        other_client = WaitingListModel.objects.order_by('pk').first()
+
+        if other_client is None:
+            raise ObjectDoesNotExist()
+
+        other_client.delete()
+        game_session = GameSessionModel.create(current_client.client_id, other_client.client_id)
+        print(game_session)
+        return HttpResponseRedirect(reverse('game:game_session', args=[game_session.session_id]))
+
+class GameSessionView(TemplateView):
+    template_name = "game/board.html"
