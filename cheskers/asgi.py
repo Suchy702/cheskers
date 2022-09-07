7f491d06d8aefafb93eaedfa65691ef1b3491626
Łukasz Leszko <leszko.lucas@gmail.com>
2022-09-07 18:54:41 +0200

    Fix heroku

--- cheskers/asgi.py
+++ cheskers/asgi.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cheskers.settings")
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cheskers.settings")
 
 import game.routing
 
