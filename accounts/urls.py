e192a27725dc1774c854d7efa517856681e726d8
Łukasz Leszko <leszko.lucas@gmail.com>
2022-09-07 12:02:39 +0200

    Add update page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
-from .views import SignUpView, ProfileView
+from .views import SignUpView, ProfileView, UpdateProfileView
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path('profile/', ProfileView.as_view(), name="profile")
+    path('profile/', ProfileView.as_view(), name="profile"),
+    path('update/<pk>', UpdateProfileView.as_view(), name="update")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
-from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 
 from cheskers.mixins import LoginForbiddenMixin
@@ -13,3 +15,11 @@ class SignUpView(LoginForbiddenMixin, generic.CreateView):
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = "registration/profile.html"
+
+class UpdateProfileView(UpdateView):
+    model = User
+    template_name = 'registration/update.html'
+    fields = ['username', 'first_name', 'last_name', 'email']
+
+    def get_success_url(self):
+        return reverse('profile')
