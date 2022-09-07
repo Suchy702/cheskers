857d31dfd49067051847f7734c949f2f1929a8c1
Łukasz Leszko <leszko.lucas@gmail.com>
2022-09-07 11:34:32 +0200

    Add profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
-from .views import SignUpView
+from .views import SignUpView, ProfileView
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path('profile/', ProfileView.as_view(), name="profile")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 from django.views import generic
-from cheskers.mixins import LoginForbiddenMixin
 
+from cheskers.mixins import LoginForbiddenMixin
 
 class SignUpView(LoginForbiddenMixin, generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "registration/profile.html"
