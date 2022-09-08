7d36d0ab5aa503cd5b6f2077df5e45932cb3a5b1
Suchy702 <wojtek.suchy6@wp.pl>
2022-09-08 16:10:34 +0200

    add checker correct movement

--- game/game_logic/jackstraws/checker.py
+++ game/game_logic/jackstraws/checker.py
@@ -5,26 +5,40 @@ from game.game_logic.logic_func import is_in_checker_board, change_pos_name, is_
 class Checker(Jackstraw):
     def __init__(self, pos: str = ''):
         super().__init__(pos, 'C')
+        self.moves = set()
+        self.beat_dirs = [(-2, 0), (-2, 2), (0, 2), (2, 2), (2, 0), (2, -2), (0, -2), (-2, -2)]
+        self.opon_dirs = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
 
-    def _add_normal_moves(self, board: dict[str], moves: list[str]) -> None:
+    def _add_normal_moves(self, board: dict[str]) -> None:
         directions = [(-1, -1), (1, -1)]
         for dirs in directions:
             new_pos = change_pos_name(self.pos, dirs[0], dirs[1])
             if is_in_checker_board(new_pos) and board[new_pos] == '.':
-                moves.append(new_pos)
+                self.moves.add(new_pos)
 
-    def _add_beat_moves(self, board: dict[str], moves: list[str]) -> None:
-        beat_dirs = [(-2, 0), (-2, 2), (0, 2), (2, 2), (2, 0), (2, -2), (0, -2), (-2, -2)]
-        opon_dirs = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+    def _add_beat_moves(self, board: dict[str], pos: str) -> None:
+        for b_dirs, o_dirs in zip(self.beat_dirs, self.opon_dirs):
+            new_beat_pos = change_pos_name(pos, b_dirs[0], b_dirs[1])
+            new_opon_pos = change_pos_name(pos, o_dirs[0], o_dirs[1])
+
+            if not is_in_checker_board(new_beat_pos):
+                continue
+
+            if board[new_beat_pos] == '.' and is_chess_on_pos(board[new_opon_pos]) and new_beat_pos not in self.moves:
+                self.moves.add(new_beat_pos)
+                self._add_beat_moves(board, new_beat_pos)
+
+
+    def get_legal_moves(self, board: dict[str]) -> list[str]:
+        self.moves = []
+        self._add_normal_moves(board)
+        self._add_beat_moves(board, pos)
+        return list(self.moves)
+
+'''
         for b_dirs, o_dirs in zip(beat_dirs, opon_dirs):
             new_beat_pos = change_pos_name(self.pos, b_dirs[0], b_dirs[1])
             new_opon_pos = change_pos_name(self.pos, o_dirs[0], o_dirs[1])
             if is_in_checker_board(new_beat_pos) and board[new_beat_pos] == '.' and is_chess_on_pos(board[new_opon_pos]):
                 moves.append(new_beat_pos)
-
-    def get_legal_moves(self, board: dict[str]) -> list[str]:
-        moves = []
-        self._add_normal_moves(board, moves)
-        self._add_beat_moves(board, moves)
-        return moves
-
+'''
